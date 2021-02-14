fc4f33978bfb046b047608ef2bbac9c8d088badd
Chris Kyriakopoulos <kiriakopoulos44@gmail.com>
2021-02-14 12:21:30 +0200

    test script

--- test.sh
+++ test.sh
@@ -6,8 +6,14 @@ for sbs in 10000 100000 1000000 10000000 100000000 1000000000; do
 
 		kill `pidof parallelFramework`
 		echo ------------------------------ cbs = $cbs ------------------------------ | tee -a results_exp.txt
-		mpirun -n 2 --bind-to none --mca btl_base_warn_component_unused 0 ./parallelFramework -gs 3 -me 3 -gpu -cbs $cbs | tee -a results_exp.txt
-		mpirun -n 2 --bind-to none --mca btl_base_warn_component_unused 0 ./parallelFramework -gs 2 -ms 4 -gpu -cbs $cbs | tee -a results_exp.txt
+
+		for m in 1 2 3 4; do
+
+			for g in 3 4 5 6; do
+				mpirun -n 2 --bind-to none --mca btl_base_warn_component_unused 0 ./parallelFramework -gs $g -ge $g -ms $m -me $m -gpu -cbs $cbs | tee -a results_exp.txt
+			done
+
+		done
 
 	done
 
